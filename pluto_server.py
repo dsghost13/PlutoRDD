b91356e14411b2dd3c5ebed09a93cd3951b1ea1e
Peter Tan Dat Le <dedsnailyghost13@gmail.com>
2026-01-14 16:13:53 -0800

    Basic TCP Test

--- pluto_server.py
+++ pluto_server.py
@@ -1,28 +1,18 @@
 import socket
-import adi
 
-SAMPLE_RATE = 1e6       # e.g., 1e6 for 1 MSPS
-CENTER_FREQ = 2.4e9     # e.g., 2.4 GHz
-BUFFER_SIZE = 1024      # number of samples per send
-
-HOST = ''               # Listen on all interfaces
-PORT = 12345            # TCP port to stream to client
-
-pluto = adi.Pluto()
-pluto.sample_rate = SAMPLE_RATE
-pluto.rx_lo = CENTER_FREQ
+HOST = '172.29.234.51'
+PORT = 12345
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 print(f"Server listening on port {PORT}...")
 
-conn, addr = server_socket.accept()
+connection, addr = server_socket.accept()
 print(f"Client connected from {addr}")
 
 try:
     while True:
-        iq_samples = pluto.rx()
-        conn.sendall(iq_samples.tobytes())
+        connection.send(bytes("Hello, World!", "utf-8"))
 finally:
-    conn.close()
+    connection.close()
